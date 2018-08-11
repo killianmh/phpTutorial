315d452646a34c3008256adad83bc6045407343f
killianmh <killianmh@gmail.com>
2018-08-10 17:52:06 -0700

    added good commenting template

---
+++ action.php
@@ -0,0 +1,26 @@
+<?php 
+//======================================================================
+// CATEGORY LARGE FONT
+//======================================================================
+
+//-----------------------------------------------------
+// Sub-Category Smaller Font
+//-----------------------------------------------------
+
+/* Title Here Notice the First Letters are Capitalized */
+
+# Option 1
+# Option 2
+# Option 3
+
+/*
+* This is a detailed explanation
+* of something that should require
+* several paragraphs of information.
+*/
+
+// This is a single line comment.
+?>
+
+Hi <?php echo htmlspecialchars($_POST['name']); ?>.
+You are <?php echo (int)$_POST['age']; ?> years old.
